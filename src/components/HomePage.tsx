2d722da2c6607f9e105e2555fc679c3221abb740
soumya tiwari <soumyatiwari7866@gmail.com>
2026-03-14 01:59:16 +0530

    git commit -m "Favourite Images Functionality with useReducer"

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useReducer, useState } from 'react';
 import SearchBar from './SearchBar';
 
 const HomePage = () => {
+
     // const [text, setText] = useState('');
     const [showImg, setShowImg] = useState(true);
     const [data, setData] = useState<any[]>([]);
@@ -9,7 +10,6 @@ const HomePage = () => {
     const [error, setError] = useState<any>('');
     const [searchData, setSearchData] = useState('');
     const [filterdData,setFilteredData] = useState<any[]>([]);
-
 
     useEffect(() => {
         setTimeout(() => {
@@ -41,6 +41,28 @@ const HomePage = () => {
         setFilteredData(filteredAuthor);
 
     }, [searchData,data])
+
+    const reducer = (state:any,action:any)=>{
+        if(action.type==='add_fav'){
+            const alreadyExists = state.find((item:any) => item.id === action.payload.id);
+
+            if (alreadyExists) {
+            alert("Already in favorites!"); 
+            return state; 
+        }
+            const favstate= [...state,action.payload];
+            localStorage.setItem('favorites',JSON.stringify(favstate));
+            return favstate
+        }
+
+        return state;
+    }
+
+    const [favImage,dispatch] = useReducer(reducer,[],()=>{
+        const saved = localStorage.getItem('favorites');
+        return saved? JSON.parse(saved):[];
+    });
+    console.log(useReducer(reducer,[]));
 
     if (error) {
         return (
@@ -85,7 +107,7 @@ const HomePage = () => {
                                             <img src={el.download_url} alt="image" className='object-cover h-full w-full' />
                                         </div>
                                         <h1>{el.author}</h1>
-                                        <svg xmlns="http://www.w3.org/2000/svg" height={20} width={20} onClick={()=>console.log(el.download_url)} viewBox="0 0 640 640"><path fill="rgb(246, 7, 7)" d="M305 151.1L320 171.8L335 151.1C360 116.5 400.2 96 442.9 96C516.4 96 576 155.6 576 229.1L576 231.7C576 343.9 436.1 474.2 363.1 529.9C350.7 539.3 335.5 544 320 544C304.5 544 289.2 539.4 276.9 529.9C203.9 474.2 64 343.9 64 231.7L64 229.1C64 155.6 123.6 96 197.1 96C239.8 96 280 116.5 305 151.1z" /></svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" height={20} width={20} onClick={()=>dispatch({type:"add_fav",payload:el})} viewBox="0 0 640 640"><path fill="rgb(246, 7, 7)" d="M305 151.1L320 171.8L335 151.1C360 116.5 400.2 96 442.9 96C516.4 96 576 155.6 576 229.1L576 231.7C576 343.9 436.1 474.2 363.1 529.9C350.7 539.3 335.5 544 320 544C304.5 544 289.2 539.4 276.9 529.9C203.9 474.2 64 343.9 64 231.7L64 229.1C64 155.6 123.6 96 197.1 96C239.8 96 280 116.5 305 151.1z" /></svg>
 
                                     </div>
                                 )
